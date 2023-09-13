952767f68cd08a4213b81da648a3ed1c4d84f50a
PeanutsBirthday <144156375+PeanutsBirthday@users.noreply.github.com>
2023-09-13 13:15:55 -0400

    Create helloCallTest.c
    sys call test

---
+++ helloCallTest.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#define NR_hello 548 long hello_syscall(void) {
+return syscall( NR_hello);
+}
+int main(int argc, char *argv[]) {
+long rv;
+rv = hello_syscall(); if(rv < 0) {
+perror("Hello syscall failed");
+}
+else {
+printf("Hello syscall ran successfully, check dmesg output\n");
+}
+return 0;
+}
