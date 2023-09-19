c323b6dfa20581c8cf00d2bd42e810f83ea23728
Kyle <kyle@debianOS.myguest.virturalbox.org>
2023-09-19 18:26:48 -0400

    correct formatting and null pntr

---
+++ memCopyTest.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+
+void successful(){
+	//int size = 100;
+        unsigned char from[100] = "Hello, World!";
+        unsigned char to[100];
+        int size = strlen(from) + 1;
+
+        long result = syscall(549, to, from, size);
+
+        if (result == 0) {
+                printf("Syscall succeeded!\n");
+                printf("Copied data from 'from': %s\n", from);
+                //for (int ii = 0; ii < size; ii++) {
+                //      printf("%c", from[ii]);
+                //}
+        printf("Copied data to 'to': %s\n", to);
+                //for (int i = 0; i < size; i++) {
+                //      printf("%c", from[i]);
+                //}
+        } else {
+        perror("Syscall failed");
+        }
+}
+//invalid size
+void sizeError(){
+        //invalid size
+        //int size = 100;
+        unsigned char from[100] = "Hello, World!";
+        unsigned char to[100];
+        int size = strlen(from) + 1;
+
+        long result = syscall(549, to, from, (size*2) );
+
+        if (result != 0) {
+                printf("Syscall failed successfully!\n");
+                printf("from: %s\n", from);
+                //for (int ii = 0; ii < size; ii++) {
+                //      printf("%c", from[ii]);
+                //}
+        printf("to: %s\n", to);
+                //for (int i = 0; i < size; i++) {
+                //      printf("%c", from[i]);
+                //}
+        } else {
+        perror("Syscall failed to fail");
+        }
+}
+void nullCopyFrom(){
+//null from or to?
+//        int size = 100;
+        unsigned char from = NULL;
+        unsigned char to[100];
+//        int size = strlen(from) + 1;
+	int size = 10;
+        long result = syscall(549, to, from, size);
+
+        if (result != 0) {
+                printf("Syscall failed successfully!\n");
+                printf("from: %s\n", from);
+                //for (int ii = 0; ii < size; ii++) {
+                //      printf("%c", from[ii]);
+                //}
+        printf("to: %s\n", to);
+                //for (int i = 0; i < size; i++) {
+                //      printf("%c", from[i]);
+                //}
+        } else {
+        perror("Syscall failed to fail");
+        }
+}
+void nullCopyTo(){
+//null from or to?
+//        int size = 100;
+        unsigned char from[100] = "Hello, World!";
+        unsigned char to[3]; 
+        int size = strlen(from) + 1;
+
+        long result = syscall(549, to, from, size);
+
+        if (result != 0) {
+                printf("Syscall failed successfully!\n");
+                printf("from: %s\n", from);
+                //for (int ii = 0; ii < size; ii++) {
+                //      printf("%c", from[ii]);
+                //}
+        printf("to: %s\n", to);
+                //for (int i = 0; i < size; i++) {
+                //      printf("%c", from[i]);
+                //}
+        } else {
+        perror("Syscall failed to fail");
+        }
+}
+void toSelf(){
+//from is to?
+        //int size = 100;
+        unsigned char from[100] = "Hello, World!"; 
+        int size = strlen(from) + 1;
+
+        long result = syscall(549, from, from, size);
+
+        if (result != 0) {
+                printf("Syscall failed successfully!\n");
+                printf("from: %s\n", from);
+                //for (int ii = 0; ii < size; ii++) {
+                //      printf("%c", from[ii]);
+                //}
+        printf("to: %s\n", from);
+                //for (int i = 0; i < size; i++) {
+                //      printf("%c", from[i]);
+                //}
+        } else {
+        perror("Syscall failed to fail");
+        }
+}
+
+
+int main() {
+        successful();
+        sizeError();
+        nullCopyFrom();
+        nullCopyTo();
+        toSelf();
+        return 0;
+}
