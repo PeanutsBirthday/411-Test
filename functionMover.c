0509ebed69e0c0484df2c811e45227f594c01e4a
PeanutsBirthday <144156375+PeanutsBirthday@users.noreply.github.com>
2023-09-19 23:39:43 -0400

    Update functionMover.c

--- functionMover.c
+++ functionMover.c
@@ -1,36 +1,46 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <linux/kernel.h>
-#include <sys/syscall.h>
+void successful(){
+        unsigned char from[100] = "Hello, World!"; // Source data
+        unsigned char to[100];                     // Destination buffer
+        int size = strlen(from) + 1;               // Include null terminator
+
+        // Initialize the destination buffer with zeros
+        //memset(to, 0, sizeof(to));
+
+        // Call the syscall to copy data from 'from' to 'to'
+        long result = syscall(549, to, from, size); 
+
+        if (result == 0) {
+                printf("Syscall succeeded!\n");
+                printf("Copied data from 'from': %s\n", from);
+                //for (int ii = 0; ii < size; ii++) {
+                //      printf("%c", from[ii]);
+                //}
+        printf("Copied data to 'to': %s\n", to);
+                //for (int i = 0; i < size; i++) {
+                //      printf("%c", from[i]);
+                //}
+        } else {
+        perror("Syscall failed");
+        }
+}
+
+
+//invalid size
+//null from or to?
+//from is to?
+//
+void error(){
+
+}
+
+
+
+
 
 int main() {
-    unsigned char from[100] = "Hello, World!"; // Source data
-    unsigned char to[100];                     // Destination buffer
-    int size = strlen(from) + 1;               // Include null terminator
-
-    // Initialize the destination buffer with zeros
-    //memset(to, 0, sizeof(to));
-
-    // Call the syscall to copy data from 'from' to 'to'
-    long result = syscall(549, to, from, size); 
-
-    if (result == 0) {
-        printf("Syscall succeeded!\n");
-        printf("Copied data from 'from': %s\n", from);
-	//for (int ii = 0; ii < size; ii++) {
-        //	printf("%c", from[ii]);
-	//}
-        printf("Copied data to 'to': %s\n", to);
-	//for (int i = 0; i < size; i++) {
-        //	printf("%c", from[i]);
-        //}
-    } else {
-        perror("Syscall failed");
-    }
-
-    return 0;
+        successful();
+        error();
+        return 0;
 }
 
 
