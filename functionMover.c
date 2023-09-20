efccef0d56260441bfeb9efea5293e27c198c69c
PeanutsBirthday <144156375+PeanutsBirthday@users.noreply.github.com>
2023-09-19 23:28:14 -0400

    Update functionMover.c

--- functionMover.c
+++ functionMover.c
@@ -5,6 +5,46 @@
 #include <linux/kernel.h>
 #include <sys/syscall.h>
 
+int main() {
+    unsigned char from[100] = "Hello, World!"; // Source data
+    unsigned char to[100];                     // Destination buffer
+    int size = strlen(from) + 1;               // Include null terminator
+
+    // Initialize the destination buffer with zeros
+    //memset(to, 0, sizeof(to));
+
+    // Call the syscall to copy data from 'from' to 'to'
+    long result = syscall(549, to, from, size); 
+
+    if (result == 0) {
+        printf("Syscall succeeded!\n");
+        printf("Copied data from 'from': %s\n", from);
+	//for (int ii = 0; ii < size; ii++) {
+        //	printf("%c", from[ii]);
+	//}
+        printf("Copied data to 'to': %s\n", to);
+	//for (int i = 0; i < size; i++) {
+        //	printf("%c", from[i]);
+        //}
+    } else {
+        perror("Syscall failed");
+    }
+
+    return 0;
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+
 #define NR_hello 548 
 
 long hello_syscall(void) {
@@ -24,22 +64,4 @@ int main(int argc, char *argv[]) {
 
 ////////////////////////////////////////////////////////////////////////////
 
-#include <linux/kernel.h>
-#include <linux/syscalls.h>
-#include <linux/uaccess.h>
-#include <linux/errno.h>
 
-//void *memcpy(void *dest, const void *src, size_t n)
-SYSCALL_DEFINE3(memory_copy, unsigned char __user *,to, unsigned char __user *, from, int, size){
-	if( access_ok(VERIFY_READ, from, size) ){
-		for (int ii = 0; ii < size; i++) {
-      		  	if (get_user(value, (from + ii) )) {
-      				return -EFAULT; // failed copy 
-      		  	}
-        		if (put_user(value, (to + ii) )) {
-            			return -EFAULT; // failed copy 
-        		}
-    		}	
-	}
-	return 0; //successful copy 
-}
